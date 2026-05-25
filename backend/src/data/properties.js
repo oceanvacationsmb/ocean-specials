a4ffa55b643f253f4c04bc1dc61470e929cdb4c8
oceanvacationsmb <oceanvacationsmb@gmail.com>
2026-05-25 00:51:18 -0400

    Create properties.js

---
+++ backend/src/data/properties.js
@@ -0,0 +1,18 @@
+export const properties = [
+  {
+    id: "827B",
+    title: "6BR Murrells Inlet Home",
+    listingId: "68db1a3f34efe70012fd1284",
+    bedrooms: 6,
+    sleeps: 18,
+    location: "Murrells Inlet",
+    sellingPoints: ["Private Pool", "Walk to Beach", "Sleeps 18"],
+    photoUrl: "https://assets.guesty.com/image/upload/v1777300444/production/68d5ff6bea1895d9005ea411/r2kpgabkalhiqka5rrzh.jpg",
+    directBookingUrl: "https://oceanvacationsmb.guestybookings.com/en/properties/68db1a3f34efe70012fd1284",
+    airbnbReviewUrl: "",
+    minNights: 2,
+    maxNights: 7,
+    scanDays: 30,
+    active: true
+  }
+];
